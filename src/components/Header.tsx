b3b9921d1c765adb49429ffb3a3ef6bf49be820c
s4nt14go <santiagopergamo@gmail.com>
2021-06-09 09:03:47 -0300

    feat: remove image placeholder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,14 @@
 import logo from "../assets/logo.png";
-import placeholder from "../assets/placeholder.png";
-import React, {useState} from "react";
+import React from "react";
 
 const img = `w-8/12 md:w-96 mx-auto inline my-6`;
 
 function Header(){
 
-  const [ imgLoaded, setImgLoaded ] = useState(false);
-
   return <section className="text-gray-600 body-font">
     <div className="flex flex-col text-center w-full mb-8 mt-7">
       <div>
-        <img alt='Civic' src={logo} className={img} onLoad={() => setImgLoaded(true)}/>
-        { !imgLoaded && <img alt='Civic' src={placeholder} className={img} /> }
+        <img alt='Civic' src={logo} className={img} />
       </div>
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Integration Exercise</h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base"><a href="https://docs.civic.com" target='_blank' rel="noreferrer" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>Reference Guide</a></p>
